a70a6428b3acf935b733f2a7ce76a0327be3cf1f
vinhptit <100837872+vinhbui212@users.noreply.github.com>
2024-07-29 10:36:02 +0700

    Update HomeController.java

--- task8_ldap/src/main/java/com/example/authenticatingldap/HomeController.java
+++ task8_ldap/src/main/java/com/example/authenticatingldap/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.authenticatingldap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ldap.core.AttributesMapper;
+import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.ldap.userdetails.LdapUserDetails;
@@ -8,22 +11,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
 
+@RestController
 public class HomeController {
+
+	@Autowired
+	private LdapTemplate ldapTemplate;
 
 	@GetMapping("/")
 	public String index(Authentication authentication) {
 		String username = authentication.getName();
+		List<String> results = search(username);
 
+		if (results.isEmpty()) {
+			return "Welcome to the home page, " + username + "! No CN found.";
+		} else {
+			return "Welcome to the home page, " + username + "! CN: " + results;
+		}
+	}
 
-		return "Welcome to the home page, " + username + "!"
-				;
+	public List<String> search(String username) {
+		return ldapTemplate.search(
+				"ou=people,dc=springframework,dc=org",
+				"(cn=" + username + ")",
+				(AttributesMapper<String>) attrs -> (String) attrs.get("cn").get()
+		);
 	}
 }
-
-
-
-
-
-
